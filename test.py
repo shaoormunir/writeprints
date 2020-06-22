f745ea867adc6984a04630c407256d81db874a46
Shaoor Munir <shaoormunir@outlook.com>
2020-06-22 05:28:14 +0500

    cleanup

--- test.py
+++
@@ -1,7 +0,0 @@
-from writeprints.text_processor import Processor
-
-processor = Processor(False)
-
-output = processor.process("This is a sample test and I am going to look at it")
-
-print(len(output.keys()))
